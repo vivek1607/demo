074f2d502105eebc9d9ec2a3ac25bb2067a82176
Vivek Kumar <vivekharit1607@gmail.com>
2023-04-10 22:19:59 +0530

    just for fun

---
+++ app.js
@@ -0,0 +1,9 @@
+let sum = (a, b) => a + b;
+console.log(sum(3, 4));
+let isPositve = (number) => number > 0;
+console.log(isPositve(-40));
+let randomNumber = () => Math.random;
+console.log(randomNumber);
+document.addEventListener("click", () => {
+  console.log("click");
+});
